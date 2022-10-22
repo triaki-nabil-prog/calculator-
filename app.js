280458cc9e2320470ad689e18f75f82ee187813e
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-23 00:37:14 +0100

    created a button event listening code
     each time a button is clicked a event will trigger
     and captured by event listener and we get the text content of
     the button that triggered the click

--- app.js
+++ app.js
@@ -29,3 +29,17 @@ function operate(operator, a, b) {
     }
 }
 
+
+
+// get the nodlist for all button nodes 
+const buttons = document.querySelectorAll('button');
+// loop through all buttons and add a event listener 
+buttons.forEach(buttonClick => buttonClick.addEventListener("click", function (buttonClick) {
+    display(buttonClick);
+    buttonClick.stopPropagation();
+}));
+// execute this function when event triggered and 
+function display(event) {
+// gets the text content of the button triggered by the event 
+    console.log(event.target.textContent);
+}
